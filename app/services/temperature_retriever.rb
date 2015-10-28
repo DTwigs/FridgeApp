8cc66bbf850932c539e83e5bd53a70ed8f906226
dan terwilliger <dan.t@theclymb.com>
2015-10-28 16:25:03 -0700

    Add methods to TemperatureRetriever and specs

--- app/services/temperature_retriever.rb
+++ app/services/temperature_retriever.rb
@@ -1,11 +1,50 @@
 class TemperatureRetriever
-  def get_temps(start_date, end_date)
+  STD_TIME_INTERVAL = 10.minute
+
+  def get_temps(start_date, end_date, add_gaps = true)
     temps = []
     if end_date.nil?
       temps = Temperature.where("created_at >= ?", start_date).order('created_at ASC')
     else
       temps = Temperature.where(:created_at => start_date..end_date).order('created_at ASC')
     end
-    temps
+    add_gaps ? fill_in_gaps(temps) : temps
+  end
+
+  def get_minute_difference(temp1, temp2)
+    time1 = temp1.created_at
+    time2 = temp2.created_at
+    (time2 - time1) / 60
+  end
+
+  def create_time_gap(mins, last_recorded_date)
+    {
+      gap: mins,
+      created_at: last_recorded_date + (mins/2).minutes
+    }
+  end
+
+  def fill_in_gaps(temps)
+    filled_in = []
+    last_temp = nil
+
+    temps.each do |t|
+      if last_temp.nil?
+        filled_in.push(t)
+      else
+        min_diff = get_minute_difference(last_temp, t)
+
+        if min_diff > (STD_TIME_INTERVAL * 1.5)
+          gap = create_time_gap(min_diff, last_temp.created_at)
+          filled_in.push(gap)
+        end
+
+        filled_in.push(t)
+        last_temp = t
+      end
+    end
+
+    filled_in
   end
 end
+
--- spec/services/temperature_retriever_spec.rb
+++ spec/services/temperature_retriever_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe TemperatureRetriever do
+  let(:tr) { TemperatureRetriever.new }
 
   describe "#get_temps" do
     let(:temp1) { Temperature.create(temperature: 35, created_at: "2015-10-05") }
@@ -8,12 +9,37 @@ describe TemperatureRetriever do
     let(:temp3) { Temperature.create(temperature: 42, created_at: "2015-10-03") }
     let(:temp4) { Temperature.create(temperature: 32, created_at: "2015-10-02") }
 
+    before do
+      temp1;temp2;temp3;temp4
+    end
+
     it 'returns a range of temperatures if start and end dates provided' do
-      expect(TemperatureRetriever.new.get_temps("2015-10-03", "2015-10-04")).to eq([temp3, temp2])
+      temps = tr.get_temps("2015-10-03", "2015-10-04", false)
+      expect(temps.count).to eq(2)
     end
 
     it 'returns all temperatures more recent than the date provided' do
-      expect(TemperatureRetriever.new.get_temps("2015-10-03", nil)).to eq([temp3, temp2, temp1])
+      expect(tr.get_temps("2015-10-03", nil, false).count).to eq(3)
+    end
+  end
+
+  describe "#get_minute_difference" do
+    let(:temp1) { Temperature.create(temperature: 35, created_at: "2015-10-05 01:30") }
+    let(:temp2) { Temperature.create(temperature: 39, created_at: "2015-10-05 01:42") }
+    let(:temp3) { Temperature.create(temperature: 42, created_at: "2015-10-05 03:17") }
+
+    it 'returns the difference between two dates in minutes' do
+
+      expect(tr.get_minute_difference(temp1, temp2)).to eq(12)
+      expect(tr.get_minute_difference(temp2, temp3)).to eq(95)
+    end
+  end
+
+  describe '#create_time_gap' do
+    it 'creates a hash with the gap of minutes and time' do
+      date = DateTime.parse("2015-10-04 01:30")
+      gap = tr.create_time_gap(32, date)
+      expect(gap).to eq({ gap: 32, created_at: date + (32/2).minutes})
     end
   end
 end
