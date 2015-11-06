94720d5ef7250f5d0a8357e983e421b3120a4cdb
dan terwilliger <dan.t@theclymb.com>
2015-11-06 14:41:42 -0800

    Add more tests for api temperature controller #index

--- spec/controllers/api/temperature_controller_spec.rb
+++ spec/controllers/api/temperature_controller_spec.rb
@@ -1,21 +1,53 @@
 require 'rails_helper'
 
-describe Api::TemperatureController do 
-  it 'shows a list of temperatures for a timespan' do 
-    temperature_data = { "temp1" => 35, "temp1" => 36 }
-    expect_any_instance_of(::TemperatureRetriever).to receive(:get_temps) { temperature_data }
-    get :index, :start_date => "2015-10-05 01:30", :end_date => "2015-10-05 03:18"
-    expect(response).to be_success
-    j = JSON.parse(response.body) #response.body is the json that returned from the controller
-    expect(j).to eql({"success" => true, "temps" => temperature_data })
+describe Api::TemperatureController do
+
+  let(:temperature_data) { { "temp1" => 35, "temp2" => 36 } }
+
+  context 'successful api calls' do
+    before do
+      expect_any_instance_of(::TemperatureRetriever).to receive(:get_temps) { temperature_data }
+    end
+
+    it 'shows a list of temperatures for a timespan' do
+      get :index, :start_date => "2015-10-05 01:30", :end_date => "2015-10-05 03:18"
+
+      expect(response).to be_success
+      j = JSON.parse(response.body) #response.body is the json that returned from the controller
+      expect(j).to eql({"success" => true, "temps" => temperature_data })
+    end
+
+    it 'shows a list of temperatures for a timespan' do
+      get :index, :start_date => "2015-10-05 01:30"
+
+      expect(response).to be_success
+      j = JSON.parse(response.body) #response.body is the json that returned from the controller
+      expect(j).to eql({"success" => true, "temps" => temperature_data })
+    end
   end
 
-  it 'returns error if invalid start date' do 
-    temperature_data = { "temp1" => 35, "temp1" => 36 }
-    allow_any_instance_of(::TemperatureRetriever).to receive(:get_temps) { temperature_data }
-    get :index, :start_date => "A Date", :end_date => "2015-10-05 03:18"
-    e = JSON.parse(response.body)
-    expect(e).to eql({"success" => false, "error" => "invalid date", "status" => "forbidden"})
+  context 'failed api calls' do
+
+    before do
+      allow_any_instance_of(::TemperatureRetriever).to receive(:get_temps) { temperature_data }
+    end
+
+    it 'returns error if invalid start date' do
+      get :index, :start_date => "A Date", :end_date => "2015-10-05 03:18"
+
+      e = JSON.parse(response.body)
+      expect(e).to eql({"success" => false, "error" => "invalid date", "status" => "forbidden"})
+    end
+
+    it 'returns error if invalid start date' do
+      get :index, :start_date => "2015-10-05 03:18", :end_date => "End Date"
+
+      e = JSON.parse(response.body)
+      expect(e).to eql({"success" => false, "error" => "invalid date", "status" => "forbidden"})
+    end
+
   end
+
+
 
 end
