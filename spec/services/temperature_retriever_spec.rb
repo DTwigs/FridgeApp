9e07a0b4377d7819e87604cbe1351fc02e622664
AlexAlt <alexandra.m.altieri@gmail.com>
2015-11-09 12:12:21 -0800

    Update all specs to pass

--- spec/services/temperature_retriever_spec.rb
+++ spec/services/temperature_retriever_spec.rb
@@ -4,10 +4,10 @@ describe TemperatureRetriever do
 
   describe "#get_temps" do
     context 'no gaps filled' do
-      let(:temp1) { Temperature.create(temperature: 35, created_at: "2015-10-04 -0800") }
-      let(:temp2) { Temperature.create(temperature: 39, created_at: "2015-10-04 -0800") }
-      let(:temp3) { Temperature.create(temperature: 42, created_at: "2015-10-03 -0800") }
-      let(:temp4) { Temperature.create(temperature: 32, created_at: "2015-10-02 -0800") }
+      let(:temp1) { Temperature.create(temperature: 35, created_at: "2015-10-04") }
+      let(:temp2) { Temperature.create(temperature: 39, created_at: "2015-10-04") }
+      let(:temp3) { Temperature.create(temperature: 42, created_at: "2015-10-03") }
+      let(:temp4) { Temperature.create(temperature: 32, created_at: "2015-10-02") }
 
       before do
         temp1;temp2;temp3;temp4
@@ -15,13 +15,14 @@ describe TemperatureRetriever do
 
       it 'returns a range of temperatures if start and end dates provided' do
         tr = TemperatureRetriever.new("2015-10-03", "2015-10-04")
-        temps = tr.get_temps(false) ### false argument being passed in this example
+        temps = tr.get_temps(false)
         expect(temps.size).to eq(2)
       end
 
       it 'returns all temperatures more recent than the date provided' do
-        tr = TemperatureRetriever.new("2015-10-03 00:00:00 -0800", nil)
+        tr = TemperatureRetriever.new("2015-10-03 00:00:00 -0800", nil) ##had to manually set timezone to avoid adding extra day in range
         temps = tr.get_temps(false)
+
         expect(temps.has_key?("2015-10-03" && "2015-10-04" && "2015-10-05")).to eq(true)
         expect(temps.first[0]).to eq("2015-10-03")
         expect(temps.max[0]).to eq(DateTime.now.strftime('%Y-%m-%d'))
@@ -29,33 +30,39 @@ describe TemperatureRetriever do
     end
 
     context 'with gaps filled in' do
-      let(:temp1) { Temperature.create(temperature: 35, created_at: "2015-10-05 01:30") }
-      let(:temp2) { Temperature.create(temperature: 39, created_at: "2015-10-05 01:42") }
-      let(:temp3) { Temperature.create(temperature: 42, created_at: "2015-10-05 03:17") }
+      let(:temp1) { Temperature.create(temperature: 35, created_at: "2015-10-05 01:30 -0800") }
+      let(:temp2) { Temperature.create(temperature: 39, created_at: "2015-10-05 01:42 -0800") }
+      let(:temp3) { Temperature.create(temperature: 42, created_at: "2015-10-05 03:17 -0800") }
 
       before do
         temp1;temp2;temp3
       end
 
       it 'returns all temperatures and timegaps between temps' do
-        tr = TemperatureRetriever.new("2015-10-05 01:29", "2015-10-05 03:18")
+        tr = TemperatureRetriever.new("2015-10-05 01:29 -0800", "2015-10-05 03:18 -0800")
         temps = tr.get_temps
-        expect(temps.size).to eq(4)
-        expect(temps.at(2)[:gap]).to eq(95)
+        temp_values = temps["2015-10-05"]
+        expect(temp_values.length).to eq(4)
+        expect(temp_values.at(2).has_value?(95)).to eq(true)
+        expect(temp_values.at(2).has_key?(:gap)).to eq(true)
       end
 
       it 'returns all temperatures and timegaps at beginning of range' do
-        tr = TemperatureRetriever.new("2015-10-05 01:09", "2015-10-05 03:18")
+        tr = TemperatureRetriever.new("2015-10-05 01:09 -0800", "2015-10-05 03:18 -0800")
         temps = tr.get_temps
-        expect(temps.size).to eq(5)
-        expect(temps.at(0)[:gap]).to eq(21)
+        temp_values = temps["2015-10-05"]
+        expect(temp_values.length).to eq(5)
+        expect(temp_values.at(0).has_key?(:gap)).to eq(true)
+        expect(temp_values.at(0).has_value?(21)).to eq(true)
       end
 
       it 'returns all temperatures and timegaps at end of range' do
-        tr = TemperatureRetriever.new("2015-10-05 01:29", "2015-10-05 03:33")
+        tr = TemperatureRetriever.new("2015-10-05 01:29 -0800", "2015-10-05 03:33 -0800")
         temps = tr.get_temps
-        expect(temps.size).to eq(5)
-        expect(temps.at(4)[:gap]).to eq(16)
+        temp_values = temps["2015-10-05"]
+        expect(temp_values.length).to eq(5)
+        expect(temp_values.at(4).has_key?(:gap)).to eq(true)
+        expect(temp_values.at(4).has_value?(16)).to eq(true)
       end
     end
   end
