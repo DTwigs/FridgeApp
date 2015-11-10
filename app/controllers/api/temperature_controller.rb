1860d6962e09a4c8eea1afdc6292b702771dd035
dan terwilliger <dan.t@theclymb.com>
2015-11-09 19:27:29 -0800

    Add an api endpoint for accepting temperature data

--- app/controllers/api/temperature_controller.rb
+++ app/controllers/api/temperature_controller.rb
@@ -8,4 +8,17 @@ class Api::TemperatureController < ApplicationController
   rescue => e
     render json: {success: false, error: e.message, status: :forbidden}
   end
+
+  def receive
+    temp_in_c = params.fetch(:temperature).to_f
+    temp_in_f = temp_in_c * 1.8 + 32
+    t = Temperature.new(temperature: temp_in_f)
+    if t.save
+      render json: {success: true}
+    else
+      render json: {success: false, error: "failed to save temperature data"}
+    end
+  rescue => e
+    render json: {success: false, error: e.message, status: :forbidden}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'main#index'
   namespace :api do
-    resources :temperature
+
+    resources :temperature, only: %w(index)
+    scope 'temperature', controller: :temperature do
+      get :receive
+    end
   end
 end
